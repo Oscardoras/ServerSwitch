0a1e2f412fa425e31d616981842abe483c36a9f9
oscar <oscardoras@gmail.com>
2020-06-26 16:57:41 +0200

    Patched.

--- src/me/oscardoras/serverswitch/ServerSwitchPlugin.java
+++ src/me/oscardoras/serverswitch/ServerSwitchPlugin.java
@@ -1,4 +1,4 @@
-package org.bukkitplugin.serverswitch;
+package me.oscardoras.serverswitch;
 
 import java.util.Collection;
 import java.util.LinkedHashMap;
@@ -7,17 +7,18 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
-import org.bukkitutils.BukkitPlugin;
-import org.bukkitutils.command.v1_15_V1.Argument;
-import org.bukkitutils.command.v1_15_V1.CommandRegister;
-import org.bukkitutils.command.v1_15_V1.CommandRegister.CommandExecutorType;
-import org.bukkitutils.command.v1_15_V1.arguments.EntitySelectorArgument;
-import org.bukkitutils.command.v1_15_V1.arguments.StringArgument;
-import org.bukkitutils.command.v1_15_V1.arguments.EntitySelectorArgument.EntitySelector;
-import org.bukkitutils.io.TranslatableMessage;
 
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+
+import me.oscardoras.spigotutils.BukkitPlugin;
+import me.oscardoras.spigotutils.command.v1_16_1_V1.Argument;
+import me.oscardoras.spigotutils.command.v1_16_1_V1.CommandRegister;
+import me.oscardoras.spigotutils.command.v1_16_1_V1.CommandRegister.CommandExecutorType;
+import me.oscardoras.spigotutils.command.v1_16_1_V1.arguments.EntitySelectorArgument;
+import me.oscardoras.spigotutils.command.v1_16_1_V1.arguments.StringArgument;
+import me.oscardoras.spigotutils.command.v1_16_1_V1.arguments.EntitySelectorArgument.EntitySelector;
+import me.oscardoras.spigotutils.io.TranslatableMessage;
 
 public class ServerSwitchPlugin extends BukkitPlugin {
 	
